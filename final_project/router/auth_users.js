126009df5691c71ef10287f8e4b8030d41b8b1ab
Anas Abdelhakim Abdelgayd <117493389+anasabdelhakim@users.noreply.github.com>
2026-01-19 22:35:30 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -43,7 +43,7 @@ regd_users.post("/login", (req, res) => {
       accessToken,
       username,
     };
-    return res.status(200).send("User successfully logged in");
+    return res.status(200).json({message: "User successfully logged in"});
   } else {
     return res
       .status(208)
